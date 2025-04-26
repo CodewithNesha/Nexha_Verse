3d341f1bbb12c285139cacd53f660c4a8bf6c178
Code-Striker1003 <codestriker1003@gmail.com>
2025-04-26 10:41:10 +0530

    Initial commit

---
+++ onkm2.js
@@ -0,0 +1,15 @@
+var typed = new Typed('.multiple-text', {
+  strings: [
+      'Initiating Nexha Protocol...',
+      "Howdy, I'm Nexha",
+      'Part human, part compiler glitch',
+      'Raised in the wild lands of STEM',
+      'CSE graduate 2K24..',
+      'Code is my Wand',
+      'Just building cool stuffs..'
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
